458a58eeee315303e33acba1f79369b1ee16bbe5
dhivyadharshini360 <sivangangaidd@gmail.com>
2025-03-04 21:44:46 +0530

    Create reverseano.java

---
+++ week3/reverseano.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Reverseano {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum=sum*10+rem;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
