9b7ac4aebf7fa9e676bbbbcbd124b99ccb266cab
dhivyadharshini360 <sivangangaidd@gmail.com>
2025-03-04 21:48:09 +0530

    Add files via upload

---
+++ week2/pattern4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class pattern4{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+for(int i=1;i<=n;i++){
+for(int k=1;k<=i-1;k++){
+System.out.print(" ");
+}
+for(int j=1;j<=(2*n)-(2*i-1);j++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
